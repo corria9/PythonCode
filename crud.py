80683c9e879d05920488acda1035fcc43537445b
Juan Armando Coria Fierros <juan.coria@neoris.com>
2022-10-15 16:49:45 -0500

    Agregando el metodo modificar()

--- crud.py
+++ crud.py
@@ -1,6 +1,3 @@
-from statistics import correlation
-
-
 class Contacto :
     def __init__(self, pNombre, apellido, telefono, correo):
         self.nombre = pNombre
@@ -9,6 +6,31 @@ class Contacto :
         self.correo = correo
     def __str__(self):
         return f"Nombre: {self.nombre} {self.apellido}, Tel: {self.telefono}, eMail: {self.correo}"
+    def getNombre(self):
+        return self.nombre
+    def getApellido(self):
+        return self.apellido  
+    def setNombre(self, pNombre):
+        self.nombre = pNombre      
+    def setApellido(self, pApellido):
+        self.apellido = pApellido
+    def setTelefono(self, pTelefono):
+        self.telefono = pTelefono
+    def setCorreo(self, pCorreo):
+        self.correo = pCorreo        
+
+def modificar():
+    informar()
+    indice = int(input("Ingrese el numero de contacto que desea Modificar: "))
+    nombre = input("Ingrese nuevo nombre: ")
+
+def borrar():
+    informar()
+    indice = int(input("Ingrese el numero de contacto que desea borrar: "))
+    print(f"Esta seguro de eliminar a {listaContactos[indice - 1].getNombre()} {listaContactos[indice - 1].getApellido()}")
+    respuesta = input("S - Borrar ### N - No Borrar")
+    if(respuesta == "s"):
+        listaContactos.remove(listaContactos[indice - 1])
 
 def agregar(): #funcion o metodo
     nombre = input("Ingrese nombre: ")
@@ -17,7 +39,7 @@ def agregar(): #funcion o metodo
     correo = input("Ingrese correo: ")
     contactoNuevo = Contacto(nombre, apellido, telefono, correo)
     listaContactos.append(contactoNuevo)
-    print(contactoNuevo)
+    #print(contactoNuevo)
 
 def informar():
     print(" ")
@@ -27,9 +49,13 @@ def informar():
     
     #for contacto in listaContactos:
     #    print(contacto)
-
+    
 #Inicio del Programa
 listaContactos = []   
+contacto1 = Contacto('Armando', 'Coria', 5539809876, 'armando.coria@gmail.com')
+contacto2 = Contacto('Juan', 'Fierros', 5509897656, 'juan.fierros@gmail.com')
+listaContactos.append(contacto1)
+listaContactos.append(contacto2)
 opcion = ' '
   
 while(opcion != 'x'):
@@ -48,5 +74,9 @@ while(opcion != 'x'):
         agregar()
     elif(opcion == 'i'):    
         informar()
+    elif(opcion == 'b'):
+        borrar()  
+    elif(opcion == 'm'):
+        modificar()      
     else:   
         print("Opcion Incorrecta")
